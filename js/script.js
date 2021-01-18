c68c91c384f907dc9f68fe6abbbcf6b3e0b76d6e
John HC <69842878+Purgatos81@users.noreply.github.com>
2021-01-18 15:31:02 +0100

    Started file and questions
    Done setting upp and finished first and second question.

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// Question 1
+var outOfStock = true;
+
+if (outOfStock === true) {
+    console.log("Out of stock");
+} else {
+    console.log("In stock");
+}
+
+// Question 2
+for (let CountTo25 = 15; CountTo25 < 26; CountTo25++) {
+    if (CountTo25 === 17 || CountTo25 === 20) 
+    console.log(CountTo25);
+}
+
+
+
+// Question 3
+
+
+// Question 4
+
+
+// Question 5
+
+
+// Question 6
+
+
+// Question 7
